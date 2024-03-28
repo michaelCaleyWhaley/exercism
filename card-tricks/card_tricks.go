b52fc7f4b66b064cfff29510b83fbdb976234b8e
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-03-28 18:39:53 +0000

    passing card_tricks

--- card-tricks/card_tricks.go
+++ card-tricks/card_tricks.go
@@ -2,7 +2,7 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	return []int{2, 6, 3}
+	return []int{2, 6, 9}
 }
 
 // GetItem retrieves an item from a slice at given position.
@@ -17,15 +17,24 @@ func GetItem(slice []int, index int) int {
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	if index >= 0 && index < len(slice) {
+		slice[index] = value
+		return slice
+	}
+	return append(slice, value)
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
-	panic("Please implement the PrependItems function")
+	return append(values, slice...)
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+
+	if index+1 <= len(slice) && index >= 0 {
+		return append(slice[:index], slice[index+1:]...)
+	}
+
+	return slice
 }
