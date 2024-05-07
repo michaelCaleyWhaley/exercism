09a59ff4cae46f1a3e430b3328887f7ecd164fa1
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-05-07 18:20:16 +0100

    find birds per week, fix bird count

--- bird-watcher/bird_watcher.go
+++ bird-watcher/bird_watcher.go
@@ -1,7 +1,5 @@
 package birdwatcher
 
-import "fmt"
-
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
@@ -15,9 +13,11 @@ func TotalBirdCount(birdsPerDay []int) int {
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
+
+	initialIndex := -7 + (week * 7)
+
 	totalBirdCount := 0
-	for i := 0; i < len(birdsPerDay); i++ {
-		fmt.Println(birdsPerDay)
+	for i := initialIndex; i < initialIndex+7; i++ {
 		totalBirdCount += birdsPerDay[i]
 	}
 	return totalBirdCount
@@ -26,5 +26,10 @@ func BirdsInWeek(birdsPerDay []int, week int) int {
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i++ {
+		if i%2 != 1 {
+			birdsPerDay[i] = birdsPerDay[i] + 1
+		}
+	}
+	return birdsPerDay
 }
