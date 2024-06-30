7123c351686d9ec25b1807cd0c8c8283aa28616b
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-06-30 18:24:45 +0100

    calc interest

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -3,26 +3,34 @@ package interest
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
 	if balance < 0 {
-		return 3.213
-	} else if balance >= 0 {
-		return 0.5
+		return float32(3.213)
+	} else if balance >= 0 && balance < 1000 {
+		return float32(0.5)
 	} else if balance >= 1000 && balance < 5000 {
-		return 1.621
+		return float32(1.621)
 	}
-	return 2.475
+	return float32(2.475)
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * float64(InterestRate(balance)/100)
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return balance + Interest(balance)
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	var noOfYears = 0
+	currentBalance := balance
+
+	for currentBalance < targetBalance {
+		noOfYears += 1
+		currentBalance = AnnualBalanceUpdate(currentBalance)
+	}
+
+	return noOfYears
 }
