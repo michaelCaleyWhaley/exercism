01308c237785b7a0077e74626321b2775b5700fb
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2024-07-09 20:40:17 +0100

    regular expression exercise

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -15,17 +15,43 @@ func IsValidLine(text string) bool {
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	re := regexp.MustCompile(`(<[\*~=\-]*>)`)
+	length := len(re.FindIndex([]byte(text))) + 1
+	splitString := re.Split(text, length)
+	return splitString
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	re := regexp.MustCompile(`(?i)".*(password).*"`)
+
+	count := 0
+	for _, line := range lines {
+		length := len(re.FindIndex([]byte(line)))
+		if length > 0 {
+			count += 1
+		}
+	}
+	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	re := regexp.MustCompile(`(end-of-line[0-9]*)`)
+	return string(re.ReplaceAll([]byte(text), []byte("")))
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	re := regexp.MustCompile(`(User[[:space:]]+[a-zA-Z0-9]+)`)
+	userNameRe := regexp.MustCompile(`(?i)([a-z]+[0-9]+)`)
+
+	for index, line := range lines {
+		match := re.Find([]byte(line))
+
+		if len(match) != 0 {
+			username := userNameRe.Find(match)
+			lines[index] = "[USR] " + string(username) + " " + line
+		}
+
+	}
+
+	return lines
 }
